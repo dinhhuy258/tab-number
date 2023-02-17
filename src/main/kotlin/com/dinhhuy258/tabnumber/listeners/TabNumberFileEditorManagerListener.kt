fd572534fe12cb4064af9faadeca01b6907da258
Devin.Fu <devinfu@outlook.com>
2023-02-17 23:34:53 +0800

    Fix a issue: tab number not show in Idea 2022.x (#3)

--- src/main/kotlin/com/dinhhuy258/tabnumber/listeners/TabNumberFileEditorManagerListener.kt
+++ src/main/kotlin/com/dinhhuy258/tabnumber/listeners/TabNumberFileEditorManagerListener.kt
@@ -1,13 +1,15 @@
 package com.dinhhuy258.tabnumber.listeners
 
 import com.dinhhuy258.tabnumber.settings.TabNumberSettingState
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileEditor.*
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 import com.intellij.openapi.fileEditor.impl.EditorWindow
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Pair
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.tabs.JBTabs
+import com.intellij.ui.tabs.TabInfo
 import com.intellij.ui.tabs.TabsListener
 
 class TabNumberFileEditorManagerListener(project: Project) : FileEditorManagerListener {
@@ -19,11 +21,18 @@ class TabNumberFileEditorManagerListener(project: Project) : FileEditorManagerLi
 
     private lateinit var openedJBTabs: JBTabs
 
+    private val log: Logger = Logger.getInstance("TabNumber")
+
     override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
         refreshTabNumber()
     }
 
     override fun fileClosed(source: FileEditorManager, file: VirtualFile) {
+        refreshTabNumber()
+    }
+
+    override fun selectionChanged(event: FileEditorManagerEvent) {
+        super.selectionChanged(event)
         refreshTabNumber()
     }
 
@@ -37,9 +46,20 @@ class TabNumberFileEditorManagerListener(project: Project) : FileEditorManagerLi
                 override fun tabsMoved() {
                     refreshTabNumber()
                 }
+
+                override fun selectionChanged(oldSelection: TabInfo?, newSelection: TabInfo?) {
+                    super.selectionChanged(oldSelection, newSelection)
+                    refreshTabNumber()
+                }
+
+                override fun tabRemoved(tabToRemove: TabInfo) {
+                    super.tabRemoved(tabToRemove)
+                    refreshTabNumber()
+                }
             })
         }
 
+        //todo:解决多编辑器下，另外的编辑器中不展示tab number的问题。原因是因为当前的openedJBTabs仅为第一个编辑器
         val files: Array<VirtualFile> = editorWindow.files
         for (index in files.indices) {
             openedJBTabs.getTabAt(index).text = (index + 1).toString() + tabNumberSettingState.tabNumberSeparator + files[index].presentableName
